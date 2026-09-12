e27b5111c081b7a74e3fb0875e72a0c8d9c3bcd7
hodaziat <hoda.ziat@fau.de>
2026-09-12 16:55:58 +0200

    add.add

---
+++ my-app-2/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './componennts/Header'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  
+    <>
+      <Header />
+
+
+    </>
+  )
+}
+
+export default App
